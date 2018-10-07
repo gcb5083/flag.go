33a30b38e0c315b0ad15752dd8e47902010ce17a
Sahil Bhanderi <sahilbhanderi@Sahils-MacBook-Pro.local>
2018-10-07 04:09:54 -0400

    Added update_score function

--- Server/backend.py
+++ Server/backend.py
@@ -10,3 +10,22 @@ app = Flask(__name__)
 @app.route('/hints', methods=['GET'])
     hints = get_hints()
     return jsonify({"score":hints})
+    
+players = {}
+flags = [][]
+hints = ""
+
+# increases player's score, removes flag (delete from flag list); returns player's score
+def update_score(player_id, lat, lng):
+    # calculate distance with lat and long, compare to all the flags to find closest flag (distance formula with player lat(x1) long(y1) and flag lat(x2) long(y2))
+    for i in range(len(flags)):
+        x = lat - flags[i][0]
+        y = lng - flag[i][1]
+        distance = sqrt(x^2 + y^2)
+        if distance <= (20/364567):
+            players[player_id] += 1
+            flags.remove(flag)
+    return players[player_id]
+
+def get_hints():
+    return hints
