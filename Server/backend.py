e004ec18ac3b3647dcf16955c63938ba5f9942f4
Geoff <gcb12@live.com>
2018-10-07 05:52:09 -0400

    Added flask implementation.

--- Server/backend.py
+++ Server/backend.py
@@ -2,30 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/gps', methods=['POST'])
-    content = request.json          #TODO
-    score = update_score(content[0], conent[1], content[2])
-    return jsonify({"score":score})
-
-@app.route('/hints', methods=['GET'])
-    hints = get_hints()
-    return jsonify({"score":hints})
-    
 players = {}
-flags = [][]
+flags = [[]]
 hints = ""
 
-# increases player's score, removes flag (delete from flag list); returns player's score
-def update_score(player_id, lat, lng):
-    # calculate distance with lat and long, compare to all the flags to find closest flag (distance formula with player lat(x1) long(y1) and flag lat(x2) long(y2))
-    for i in range(len(flags)):
-        x = lat - flags[i][0]
-        y = lng - flag[i][1]
-        distance = sqrt(x^2 + y^2)
-        if distance <= (20/364567):
-            players[player_id] += 1
-            flags.remove(flag)
-    return players[player_id]
+@app.route('/gps', methods=['POST'])
+def update_score():
+	content = request.json
+	passeddata = content.split('\t')
+	passeddata[0] = player_id
+	passeddata[1] = lat
+	passeddata[2] = lng
+    	# calculate distance with lat and long, compare to all the flags to find closest flag (distance formula with player lat(x1) long(y1) and flag lat(x2) long(y2))
+	for i in range(len(flags)):
+		x = lat - flags[i][0]
+		y = lng - flag[i][1]
+		distance = sqrt(x^2 + y^2)
+		if distance <= (20/364567):
+			players[player_id] += 1
+			flags.remove(flag)
+	return jsonify({"score":players[player_id]})
 
+@app.route('/hints', methods=['GET'])
 def get_hints():
-    return hints
+	return jsonify({"score":hints})
